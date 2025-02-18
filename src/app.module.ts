bb23f31156bd746373acccd29c562fc01cb09877
SergBily <sergbily.dev@gmail.com>
2025-02-18 16:11:43 +0300

    refactor: update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
-import { promises as fs } from 'fs';
-import * as path from 'path';
-
 import { Logger, Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 
 import { DatabaseModule } from './database/database.module';
 import { CamerasModule } from './modules/cameras/cameras.module';
 import { MediaModule } from './modules/media/media.module';
 import { StorageModule } from './modules/storage/storage.module';
+import { CameraRecoveryService } from './camera-recovery.service';
+import { DirectoryService } from './directory.service';
 
 @Module({
   imports: [
@@ -17,32 +16,18 @@ import { StorageModule } from './modules/storage/storage.module';
     MediaModule,
     StorageModule,
   ],
+  providers: [CameraRecoveryService, DirectoryService],
 })
 export class AppModule {
   private readonly logger = new Logger(AppModule.name);
 
-  constructor(private readonly configService: ConfigService) {}
+  constructor(
+    private readonly directoryService: DirectoryService,
+    private readonly cameraRecoveryService: CameraRecoveryService,
+  ) {}
 
   async onModuleInit() {
-    const imagesDir = this.configService.get<string>(
-      'IMAGES_DIR',
-      path.join(__dirname, '..', 'images'),
-    );
-    const videosDir = this.configService.get<string>(
-      'VIDEOS_DIR',
-      path.join(__dirname, '..', 'videos'),
-    );
-
-    await this.createDirectory(imagesDir);
-    await this.createDirectory(videosDir);
-  }
-
-  private async createDirectory(dir: string): Promise<void> {
-    try {
-      await fs.mkdir(dir, { recursive: true });
-      this.logger.log(`Directory created or already exists: ${dir}`);
-    } catch (error) {
-      this.logger.error(`Error creating directory ${dir}: ${error.message}`);
-    }
+    await this.directoryService.createDirectories();
+    // this.cameraRecoveryService.recoverActiveCameras();
   }
 }
